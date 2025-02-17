1566797dfb3d2f37fa8e2354ae94d3db13e0d264
Clover <thewuheng@gmail.com>
2025-02-17 20:26:21 +0800

    init: Java后端开发知识点回顾、算法

---
+++ src/Learning.java
@@ -0,0 +1,19 @@
+public class Learning {
+    public static void main(String[] args) {
+        boolean isUnderstood = false;
+        while (isHasNextSolution()) {
+            if (isUnderstood) {
+                // 补习知识点
+            } else {
+                // 多看几遍，搞懂搞透思想
+                // 自己做一遍
+                isUnderstood = true;
+            }
+
+        }
+    }
+
+    private static boolean isHasNextSolution() {
+        return true;
+    }
+}
