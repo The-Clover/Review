7e6c6809731105e9b234abba9164963f29b3e0e5
Andrew Ng <thewuheng@gmail.com>
2025-02-23 21:10:16 +0800

    fix: 误删最长连续序列Solution2

---
+++ src/leetcode/hot100/_2_longestConsecutiveSequence/SolutionTwo.java
@@ -0,0 +1,37 @@
+package leetcode.hot100._2_longestConsecutiveSequence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-100-liked">最长连续序列</a>
+ * <p>1、根据示例得到存在重复可能，重复元素不构成连续，故先去重</p>
+ * <p>2、HashSet无序，所以在拼接连续序列的时候，可能会取到重复的元素，在遍历的时候还需要再次判重</p>
+ * <p>3、记录上一次的序列长度，每次断序的时候都比较更新更大的值</p>
+ */
+public class SolutionTwo {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (Integer num : numSet) {
+            // 先把下行的路堵死
+            if (!numSet.contains(num - 1)) {
+                int last = num;
+                while (numSet.contains(++last)) {
+                }
+                maxLength = Math.max(maxLength, last - num);
+            }
+        }
+
+        return maxLength;
+    }
+}
