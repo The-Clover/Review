307664435f041fee533cf34dcc5f6d148de8bf35
Clover <thewuheng@gmail.com>
2025-05-27 11:55:08 +0800

    update: hot100 第一题笔记，总结笔记

--- src/leetcode/hot100/_0_twoSum/SolutionOne.java
+++ src/leetcode/hot100/_0_twoSum/SolutionOne.java
@@ -6,6 +6,8 @@ import java.util.Map;
 /**
  * <a href="https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked">两数之和</a>
  * <p>用Hash表的空间换时间，性能更好</p>
+ * <p>没有一开始就将所有的值都存到Map中，避免了不必要的运算</p>
+ * <p>题目本身不要求找出所有的可能性，所以哈希冲突不影响，如果要求找出所有的可能性，哈希结构不适应</p>
  */
 class SolutionOne {
     public int[] twoSum(int[] nums, int target) {
